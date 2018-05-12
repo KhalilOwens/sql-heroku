714c1bf0191b876b271e5cec159903aeb7d706df
Khalil Owens <khllowens@aol.com>
2018-05-12 00:29:19 -0400

    anotcommit

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,32 @@
 var mysql = require("mysql");
 var connection;
 
+
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({ 
-        host: 'localhost',
-        user: 'root',
-        password: 'Eagles#1',
-        database:'JawsDB'
+    
+        host: 'ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',  
+        user: 't1a2aui40xaqilel',
+        password: 'ajfnub3zg7md8b3z',
+        port: 3306,
+        database: 'qngb927xf9chlw6e'
     } );
+
+
+
+    connection.connect(function(err) {
+        if (err) {
+          console.error("error connecting: " + err);
+          return;
+        }
+        console.log("connected as id " + connection);
+      });
 
 
 };
 
-connection.connect();
+
 module.exports = connection;
